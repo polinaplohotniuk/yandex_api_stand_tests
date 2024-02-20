f5362876a6579bddb9020178c7c6d8f8d801ebd3
Полина Плохотнюк <polinaplohotnuk@iMac-Sergej.local>
2024-02-20 11:41:35 +0200

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://6776e5f5-5034-4360-92ab-a7eeea80e4f5.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
